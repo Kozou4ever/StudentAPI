7f44f38c62465f692d66dec189b3245664312cdc
Kozou <louisfloquey@gmail.com>
2023-02-01 14:31:50 +0100

    feat(database): add database config

---
+++ config/db.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var database *gorm.DB
+var e error
+
+func DatabaseInit() {
+	host := "localhost"
+	user := "louis"
+	password := "123"
+	dbName := "studentdb"
+	port := 5432
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Asia/Jakarta", host, user, password, dbName, port)
+	database, e = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if e != nil {
+		panic("Failed to connect database...")
+	}
+}
+
+func DB() *gorm.DB {
+	return database
+}
